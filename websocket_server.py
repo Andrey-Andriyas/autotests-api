1fc1a4672de31002293497dae5beee8b69d063ee
Andrey-Andriyas <andriyas@mail.ru>
2025-09-12 16:45:05 +0700

    Знакомство с WebSocket протоколом

--- websocket_server.py
+++ websocket_server.py
@@ -1,18 +1,19 @@
-import asyncio
+import asyncio     # Импортируем asyncio для работы с асинхронными операциями
 
-import websockets
+import websockets   # Импортируем библиотеку для работы с WebSockets
 from websockets import ServerConnection
 
-
-async def echo(websocket: ServerConnection):
+# Обработчик входящих сообщений
+async def echo(websocket: ServerConnection):     # Асинхронно обрабатываем входящие сообщения
     async for message in websocket:
-        print(f"Получено сообщение: {message}")
-        response = f"Сервер получил: {message}"
+        print(f"Получено сообщение: {message}")     # Логируем полученное сообщение
+        response = f"Сервер получил: {message}"     # Формируем ответное сообщение
 
         for _ in range(5):
-            await websocket.send(response)
+            await websocket.send(response) # Отправляем ответ клиенту
 
 
+# Запуск WebSocket-сервера на порту 8765
 async def main():
     server = await websockets.serve(echo, "localhost", 8765)
     print("WebSocket сервер запущен на ws://localhost:8765")
